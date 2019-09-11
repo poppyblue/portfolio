f70301eac2df9cb5928255010772fb2fb8074ccb
Default name <2894519895@qq.com>
2019-09-11 19:36:17 +0800

    添加了照片和简历，修改了.gitgnore文件

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,9 +119,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# 静态
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/'),
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# 动态
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
