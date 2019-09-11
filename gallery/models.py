c2d9ceb3a6a74ee1b7b1156b65f9521389d6357c
Default name <2894519895@qq.com>
2019-09-11 15:16:28 +0800

    完善了Grallery app
    设置好了Media路径和网址，能显示图片了

---
+++ gallery/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Gallery(models.Model):
+	description = models.CharField(default = '作品简介', max_length = 100) 
+	image = models.ImageField(default = 'default.png', upload_to = 'images/')
+	title = models.CharField(default = '作品标题', max_length = 100)
+    
+	def __str__(self):
+		return self.title
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 自己的app
+    'gallery.apps.GalleryConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_hans'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
